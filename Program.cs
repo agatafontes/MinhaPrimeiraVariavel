390f6380ceccc43d9b79d6aef48f3f169cae606e
Fontes <fontesgomesagata@gmail.com>
2026-09-16 16:55:52 -0300

    cria variavel para usar depois

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// Este programa deverá salvar o nome do usuario dentro de uma variavel
+//Este programa deverá exibir uma mensagem de saudação mostrando o nome do usuário
+var nomeUsuario = "Gomes";
+Console.Write("Boas Vindas, ");
+Console.WriteLine(nomeUsuario);
